bbefa99bc8e18e009423ff45dd218a37576f95f1
ManoelPatrocinioXT <manoel.patrocinio@xtesting.com.br>
2024-10-19 19:03:51 -0300

    merge e resolução de conflitos com as branchs do Ricardo e Brenndol, update keywords.ts

--- editor-low-code/src/extension.ts
+++ editor-low-code/src/extension.ts
@@ -1,171 +1,120 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { main,form_sendEmail,form_ClicarNoElemento,form_ColetarDataAtual,form_CriarArquivo,form_EsperarElemento,form_FiltrarEmails,form_SelecionarArquivo,form_acessarSMTP, form_ExecutarExpressaoPython, form_InserirTexto, form_VoltarPara, form_IrPara } from './pages/webview';
-import { SendEmailWithRobot } from './utils/send_email_class';
-import { robotKeywords } from './robot/keywords';
-
+import {
+  main,
+  form_sendEmail,
+  form_ClicarNoElemento,
+  form_ColetarDataAtual,
+  form_CriarArquivo,
+  form_EsperarElemento,
+  form_FiltrarEmails,
+  form_SelecionarArquivo,
+  form_acessarSMTP,
+  form_ExecutarExpressaoPython,
+  form_InserirTexto,
+  form_VoltarPara,
+  form_IrPara,
+} from "./pages/webview";
+import { robotKeywords } from "./robot/keywords";
 
 export function activate(context: vscode.ExtensionContext) {
-	let panel: vscode.WebviewPanel | undefined;
+  let panel: vscode.WebviewPanel | undefined;
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('editor-low-code.start', () => {
-		
-			panel = vscode.window.createWebviewPanel(
-				'start',
-				'Editor Low Code',
-				vscode.ViewColumn.One,
-				{
-					enableScripts: true
-				}
-			);
-			// Chama a função com o html para exibir a tela inicial
-			panel.webview.html = main();
-	
-	
-			panel.webview.onDidReceiveMessage(
-				async (message) => {
-					if (message.command === 'goToHome') {
-						panel!.webview.html = main();
-					}
-					else if(message.command === 'openFilterEmailForm'){
-						panel!.webview.html = form_FiltrarEmails();
-					}
-					else if(message.command === 'openAcessarSMTPForm'){
-						panel!.webview.html = form_acessarSMTP();
-					}
-					else if(message.command === 'openColetarDataAtualForm'){
-						panel!.webview.html = form_ColetarDataAtual();
-					}
-					else if(message.command === 'openCopiarArquivoForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openMoverArquivoForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openCriarArquivoForm'){
-						panel!.webview.html = form_CriarArquivo();
-					}
-					else if(message.command === 'openLerArquivoForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openExecutPythonForm'){
-						panel!.webview.html = form_ExecutarExpressaoPython();
-					}
-					else if(message.command === 'openAbrirNavegadorForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openInserirTextoForm'){
+  context.subscriptions.push(
+    vscode.commands.registerCommand("editor-low-code.start", () => {
+      panel = vscode.window.createWebviewPanel(
+        "start",
+        "Editor Low Code",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
+      // Chama a função com o html para exibir a tela inicial
+      panel.webview.html = main();
 
-						 panel!.webview.html = form_InserirTexto();
-					}
-					else if(message.command === 'openColetarTextoForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openClicarElementoForm'){
-						panel!.webview.html = form_ClicarNoElemento();
-					}
-					else if(message.command === 'openEsperarElementoForm'){
-						panel!.webview.html = form_EsperarElemento();
-					}
-					else if(message.command === 'openSelecionarArquivoForm'){
-						panel!.webview.html = form_SelecionarArquivo();
-					}
-					else if(message.command === 'openColetarElementosForm'){
-						// panel!.webview.html = main();
-					}
-					else if(message.command === 'openIrParaForm'){
-						 panel!.webview.html = form_IrPara();
-					}
-					else if(message.command === 'openVoltarParaForm'){
-						 panel!.webview.html = form_VoltarPara(); 
-
-					else if (message.command === 'openSendEmailForm') {
-						panel!.webview.html = form_sendEmail();
-					} 
-					else if (message.command === 'modal_sendEmail') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Enviar E-mail",panel!);
-			
-					}else if (message.command === 'modal_acessoSMTP') {
-
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Acessar SMTP",panel!);
-					}
-					else if (message.command === 'modal_clickelement') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Clicar no Elemento",panel!);
-					}
-					else if (message.command === 'modal_express') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Executar Expressao Python",panel!);
-					}
-					else if (message.command === 'modal_createfile') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Criar Arquivo",panel!);
-					}
-					else if (message.command === 'modal_esperarelemento') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Esperar Elemento",panel!);
-					}
-					else if (message.command === 'modal_selecionararquivo') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Selecionar Arquivo",panel!);
-					}
-					else if (message.command === 'modal_filtraremails') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Filtrar E-mails",panel!);
-					}
-					else if (message.command === 'modal_coletardata') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Coletar Data Atual",panel!);
-					else if (message.command === 'modal_inserirtexto') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Inserir Texto",panel!);
-					}
-					else if (message.command === 'modal_irpara') {
-						// função para abrir o modal, deve se passado como 1º parâmetro a chave correspondente ao código robot que será exibido, presente na variável 'robotKeywords' 
-						await Modal("Ir Para",panel!);
-					}
-					else if (message.command === 'modal_voltarpara') {
-						await Modal("Voltar Para",panel!);
-					}
-				},
-				undefined,
-				context.subscriptions
-			);
-			
-			
-	
-		})
-	);
+      panel.webview.onDidReceiveMessage(
+        async (message) => {
+          if (message.command === "goToHome") {
+            panel!.webview.html = main();
+          } else if (message.command === "openFilterEmailForm") {
+            panel!.webview.html = form_FiltrarEmails();
+          } else if (message.command === "openAcessarSMTPForm") {
+            panel!.webview.html = form_acessarSMTP();
+          } else if (message.command === "openColetarDataAtualForm") {
+            panel!.webview.html = form_ColetarDataAtual();
+          } else if (message.command === "openCopiarArquivoForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openMoverArquivoForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openCriarArquivoForm") {
+            panel!.webview.html = form_CriarArquivo();
+          } else if (message.command === "openLerArquivoForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openExecutPythonForm") {
+            panel!.webview.html = form_ExecutarExpressaoPython();
+          } else if (message.command === "openAbrirNavegadorForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openInserirTextoForm") {
+            panel!.webview.html = form_InserirTexto();
+          } else if (message.command === "openColetarTextoForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openClicarElementoForm") {
+            panel!.webview.html = form_ClicarNoElemento();
+          } else if (message.command === "openEsperarElementoForm") {
+            panel!.webview.html = form_EsperarElemento();
+          } else if (message.command === "openSelecionarArquivoForm") {
+            panel!.webview.html = form_SelecionarArquivo();
+          } else if (message.command === "openColetarElementosForm") {
+            // panel!.webview.html = main();
+          } else if (message.command === "openIrParaForm") {
+            panel!.webview.html = form_IrPara();
+          } else if (message.command === "openVoltarParaForm") {
+			await Modal("Voltar Para", panel!);
+          } else if (message.command === "openSendEmailForm") {
+            panel!.webview.html = form_sendEmail();
+          } else if (message.command === "modal_sendEmail") {
+            await Modal("Enviar E-mail", panel!);
+          } else if (message.command === "modal_acessoSMTP") {
+            await Modal("Acessar SMTP", panel!);
+          } else if (message.command === "modal_clickelement") {
+            await Modal("Clicar no Elemento", panel!);
+          } else if (message.command === "modal_express") {
+            await Modal("Executar Expressao Python", panel!);
+          } else if (message.command === "modal_createfile") {
+            await Modal("Criar Arquivo", panel!);
+          } else if (message.command === "modal_esperarelemento") {
+            await Modal("Esperar Elemento", panel!);
+          } else if (message.command === "modal_selecionararquivo") {
+            await Modal("Selecionar Arquivo", panel!);
+          } else if (message.command === "modal_filtraremails") {
+            await Modal("Filtrar E-mails", panel!);
+          } else if (message.command === "modal_coletardata") {
+            await Modal("Coletar Data Atual", panel!);
+          } else if (message.command === "modal_inserirtexto") {
+            await Modal("Inserir Texto", panel!);
+          } else if (message.command === "modal_irpara") {
+            await Modal("Ir Para", panel!);
+          } else if (message.command === "modal_voltarpara") {
+            await Modal("Voltar Para", panel!);
+          }
+        },
+        undefined,
+        context.subscriptions
+      );
+    })
+  );
 }
 
+async function Modal(keyword: string, panel: vscode.WebviewPanel) {
+  const keywordData = robotKeywords[keyword];
+  const keywordCode = keywordData.code; // Pega o código da keyword
 
-async function Modal(keyword:string,panel: vscode.WebviewPanel) {
-    const keywordData = robotKeywords[keyword]; 
-    const keywordCode = keywordData.code; // Pega o código da keyword
-
-
-    // Envia uma mensagem para o webview exibir o modal com o código da keyword
-    panel.webview.postMessage({
-        command: 'showModal',
-        code: keywordCode // Passa o código da keyword para ser exibido no modal
-    });
-	
+  // Envia uma mensagem para o webview exibir o modal com o código da keyword
+  panel.webview.postMessage({
+    command: "showModal",
+    code: keywordCode, // Passa o código da keyword para ser exibido no modal
+  });
 }
-async function sendEmail(to: string, subject: string, text: string, extensionPath: string) {
-   
-
-    const emailSender = new SendEmailWithRobot();
-
-    try {
-        emailSender.runSendEmail(to, subject, text, extensionPath);
-    } catch (error) {
-        console.error('Erro ao enviar email:', error);
-    }
-}
-
-
 // This method is called when your extension is deactivated
 export function deactivate() {}
--- editor-low-code/src/robot/keywords.ts
+++ editor-low-code/src/robot/keywords.ts
@@ -1,119 +1,228 @@
-
 type RobotKeyword = {
-    code: string;
-    arguments: string[];
+  code: string;
+  arguments: string[];
 };
 
 export type RobotKeywords = Record<string, RobotKeyword>;
 
 // Exemplo de códigos das keywords
-export const robotKeywords:RobotKeywords = {
-    'Enviar E-mail': {
-        code: `
+export const robotKeywords: RobotKeywords = {
+  "Enviar E-mail": {
+    code: `
         *** Settings ***
         Library    RPA.Browser.Selenium
         Library    RPA.Email.ImapSmtp
 
         *** Variables ***
         
-        ${'{email}'}    
-        ${'{assunto}'}    
-        ${'{mensagem}'}
+        ${"${email}"}    
+        ${"${assunto}"}    
+        ${"${mensagem}"}
     
 
         *** Keywords ***
         Enviar E-mail
-            [Arguments]    ${'{email}'}    ${'{assunto}'}    ${'{mensagem}'}
-            Send Email    ${'{email}'}    ${'{assunto}'}    ${'{mensagem}'}`,
-        arguments: ['email', 'assunto', 'mensagem']
-    },
-    'Acessar SMTP': {
-        code: `
+            [Arguments]    ${"${email}"}    ${"${assunto}"}    ${"${mensagem}"}
+            Send Email    ${"${email}"}    ${"${assunto}"}    ${"${mensagem}"}`,
+    arguments: ["email", "assunto", "mensagem"],
+  },
+  "Acessar SMTP": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
         Library    RPA.Email.ImapSmtp
 
+        *** Variables ***
+
+        ${"${email_smtp}"}         exemple
+        ${"${senha_smtp}"}         exemple
+        ${"${servidor_smtp}"}      exemple
+        ${"${porta_smtp}"}         exemple
+
         *** Keywords ***
+
         Acessar SMTP
-        Authorize Smtp    account=${'{email_smtp}'}    password=${'{senha_smtp}'}   smtp_server=${'{servidor_smtp}'}   smtp_port${'{porta_smtp}'}`,
-        arguments: ['email_smtp', 'senha_smtp', 'servidor_smtp', 'porta_smtp']
-    },
-    'Filtrar E-mails': {
-        code: `
+        Authorize Smtp    account=${"${email_smtp}"}    password=${"${senha_smtp}"}   smtp_server=${"${servidor_smtp}"}   smtp_port${"${porta_smtp}"}
+    `,
+    arguments: ["email_smtp", "senha_smtp", "servidor_smtp", "porta_smtp"],
+  },
+  "Filtrar E-mails": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
         Library    RPA.Email.ImapSmtp
+        Library    Collections
         
+        *** Variables ***
+
+        ${"${filtro_emails}"}         exemple
+
+
         *** Keywords ***
+
         Filtrar E-mails
-            List Messages    criterion=${'{filtro_emails}'}`,
-         arguments: ['filtro_emails']
-    },
-    'Clicar no Elemento': {
-        code: `
+            List Messages    criterion=${"${filtro_emails}"}
+    `,
+    arguments: ["filtro_emails"],
+  },
+  "Clicar no Elemento": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+
+        *** Variables ***
+
+        ${"${locator}"}         exemple
+
         
         *** Keywords ***
+
         Clicar no Elemento
-            Click Element    locator=${'{locator}'}`,
-         arguments: ['locator']
-    },
-    'Esperar Elemento': {
-        code: `
+            Click Element    locator=${"${locator}"}
+    `,
+    arguments: ["locator"],
+  },
+  "Esperar Elemento": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+
+        *** Variables ***
+
+        ${"${locator}"}         exemple
+        ${"${timeout}"}         exemple
+        ${"${error_msg}"}       exemple
         
         *** Keywords ***
+
         Esperar Elemento
-            Wait Until Page Contains Element    locator=${'{locator}'}  timeout=${'{timeout}'}  error=${'{error_msg}'}`,
-         arguments: ['locator', 'timeout', 'error_msg']
-    },
-    'Selecionar Arquivo': {
-        code: `
+            Wait Until Page Contains Element    locator=${"${locator}"}  timeout=${"${timeout}"}  error=${"${error_msg}"}
+    `,
+    arguments: ["locator", "timeout", "error_msg"],
+  },
+  "Selecionar Arquivo": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+        Library    OperatingSystem
+
+        *** Variables ***
+
+        ${"${locator}"}           exemple
+        ${"${file_path}"}         exemple
         
         *** Keywords ***
         Selecionar Arquivo
-            Choose File    locator=${'{locator}'}  file_path=${'{file_path}'}`,
-         arguments: ['locator', 'file_path']
-    },
-    'Coletar Data Atual': {
-        code: `
+            Choose File    locator=${"${locator}"}  file_path=${"${file_path}"}
+            
+    `,
+    arguments: ["locator", "file_path"],
+  },
+  "Coletar Data Atual": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+        Library    DateTime
+
+        *** Variables ***
+
+        ${"${formato_data}"}           exemple
         
         *** Keywords ***
+
         Coletar Data Atual
-            ${'{data_atual}'}    Get Current Date  result_format=${'{formato_data}'}`,
-         arguments: ['formato_data']
-    },
-    'Criar Arquivo': {
-        code: `
+            ${"${data_atual}"}    Get Current Date  result_format=${"${formato_data}"}
+    `,
+    arguments: ["formato_data"],
+  },
+  "Criar Arquivo": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+        Library    OperatingSystem
+
+        *** Variables ***
+
+        ${"${arquivo}"}            exemple
+        ${"${conteudo}"}           exemple
         
         *** Keywords ***
+
         Criar Arquivo
-            Create File    path=${'{arquivo}'}  content=${'{conteudo}'}`,
-         arguments: ['arquivo', 'conteudo']
-    },
-    'Executar Expressao Python': {
-        code: `
+            Create File    path=${"${arquivo}"}  content=${"${conteudo}"}
+    `,
+    arguments: ["arquivo", "conteudo"],
+  },
+  "Executar Expressao Python": {
+    code: `
         *** Settings ***
+
         Library    RPA.Browser.Selenium
-        Library    RPA.Email.ImapSmtp
+
+        *** Variables ***
+
+        ${"${expressao}"}            exemple
         
         *** Keywords ***
+
         Executar Expressao Python
-            ${'{result_expressao}'}    Evaluate    expression=${'{expressao}'}`,
-         arguments: ['expressao']
-    },
-      // Adicione outras keywords e seus argumentos aqui conforme necessário
+            ${"${result_expressao}"}    Evaluate    expression=${"${expressao}"}
+    `,
+    arguments: ["expressao"],
+  },
+  "Inserir Texto": {
+    code: `
+        *** Settings ***
+
+        Library    RPA.Browser.Selenium
+
+        *** Variables ***
+
+        ${"${locator}"}            exemple
+        ${"${texto}"}              exemple
+        
+        *** Keywords ***
+
+        Inserir Texto
+            Input Text    locator=${"${locator}"}    text=${"${texto}"}
+        `,
+    arguments: ["locator", "texto"],
+  },
+
+  "Ir Para": {
+    code: `
+        *** Settings ***
+
+        Library    RPA.Browser.Selenium
+
+        *** Variables ***
+
+        ${"${url}"}            exemple
+        
+        *** Keywords ***
+
+        Ir Para
+            Go To    url=${"${url}"}
+        `,
+    arguments: ["url"],
+  },
+  "Voltar Para": {
+    code: `
+        *** Settings ***
+        Library    RPA.Browser.Selenium
+        
+        *** Keywords ***
+        Voltar Para
+            Go Back
+        `,
+    arguments: [],
+  },
+  // Adicione outras keywords e seus argumentos aqui conforme necessário
 };
